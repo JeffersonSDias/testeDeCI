ce71c0c5400f178512ed0b628bae02b16ed3212e
JeffersonSDias <jefferson.94silvadias@gmail.com>
2020-07-29 21:05:38 -0300

    Teste com formato do Medium

---
+++ src/test/java/SeleniumGitCI.java
@@ -0,0 +1,49 @@
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumGitCI {
+	private WebDriver driver;
+
+	@BeforeAll
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+		driver.navigate().to("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+	}
+
+	@Test
+	public void userLogin() {
+		WebElement searchTxt = driver.findElement(By.name("q"));
+		searchTxt.sendKeys("automation");
+		WebElement submitBtn = driver.findElement(By.name("btnK"));
+		submitBtn.click();
+		System.out.println("Current URL is:" + driver.getCurrentUrl());
+		Assertions.assertTrue(driver.getTitle().contains("automation - Google Search"));
+		System.out.println("Current Title is:" + driver.getTitle());
+	}
+
+	@AfterAll
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+}
